fbbfde4dab66bbd03094109c1312cc44a141bb46
ousama lasri <oussama.lasri.2k@gmail.com>
2026-04-20 20:20:18 +0100

    tes : add application ressource for testing

--- src/test/java/com/oussma/userinfo/UserinfoApplicationTests.java
+++ src/test/java/com/oussma/userinfo/UserinfoApplicationTests.java
@@ -1,12 +1,15 @@
 package com.oussma.userinfo;
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") 
 class UserinfoApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 }
